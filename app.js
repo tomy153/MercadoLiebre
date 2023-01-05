8612dfa928eb1dbdbcbf2d50317a466856431787
tomy153 <tmeglia@gmail.com>
2023-01-05 17:53:36 -0300

    retoques

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
+const port = process.env.PORT || 3001;
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+app.listen(port, ()=> console.log('Servidor corriendo en el puerto ${port}'));
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
